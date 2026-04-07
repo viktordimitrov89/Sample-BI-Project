50a48db11cc2a7c3ff8319488adb12ffa1a30b37
Viktor Dimitrov <viktordimitrov89@gmail.com>
2026-04-07 15:57:01 +0300

    Add files via upload

---
+++ sql/Question4_SampleProject.sql
@@ -0,0 +1,15 @@
+--Payhawk Associate BI Analyst Interview Project
+--Author: Viktor Dimitrov
+--Date: 07 of April 2026
+-- Description: Financial analysis of company spending patterns
+-- ================================================
+-- Question 4: Declined Transactions by Company and Employee
+-- ================================================
+
+SELECT
+DISTINCT c.company_name, e.name, t.status
+FROM transactions t
+JOIN companies c ON t.company_id=c.company_id
+JOIN employees e ON t.card_id=e.card_id
+WHERE t.status ='Declined'
+ORDER BY c.company_name, e.name;
